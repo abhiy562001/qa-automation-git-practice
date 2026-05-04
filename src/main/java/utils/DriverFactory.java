bb1506dba7af3addca02a340fe4b48b78d957698
abhiy562001 <161271427+abhiy562001@users.noreply.github.com>
2026-05-05 00:38:50 +0530

    Update DriverFactory.java

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -8,6 +8,7 @@ public class DriverFactory {
     public static WebDriver getDriver() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        System.out.println("Maximaize browser");
         return driver;
     }
 }
